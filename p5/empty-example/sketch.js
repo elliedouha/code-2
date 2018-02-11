b5653558981ffc28e8415d7db58f1c1cb0a3bf81
elliedlouha <ellie.dlouha@hotmail.com>
2018-02-11 15:37:38 -0500

    class pong collider

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -2,10 +2,14 @@ function setup() {
   // put setup code here
   createCanvas(600,600);
   background(255);
+
+  console.log('howdy');
 }
 
 function draw() {
   // put drawing code here
+
+
   
   translate(mouseX, mouseY);
   rotate(frameCount);
